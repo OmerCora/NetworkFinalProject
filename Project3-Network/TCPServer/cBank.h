7d4eed254c4afeea9770f332a0c0e353b8535a06
jaehuhn <jaehuhn@gmail.com>
2016-12-02 11:10:56 -0500

    Revert "Bank class added"
    This reverts commit c2e1d5aeee01b97592454be6cd7e4003397d2c75.

--- Project3-Network/TCPServer/cBank.h
+++ Project3-Network/TCPServer/cBank.h
@@ -1,16 +1,1 @@
 #pragma once
-#include "cPlayer.h"
-
-class cBank
-{
-public:
-	cBank();
-	~cBank();
-
-	void giveMoney(cPlayer toWhom, unsigned int howmuch);
-	void takeMoney(cPlayer fromWhom, unsigned int howmuch);
-
-private:
-	unsigned int m_money;
-
-};
