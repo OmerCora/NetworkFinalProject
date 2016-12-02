38f7b2faf8355c9b0f4083d67e3f8d96aad95172
Benjamin <congakji@gmail.com>
2016-12-02 11:52:54 -0500

    recover bank

---
+++ Project3-Network/TCPServer/cBank.cpp
@@ -0,0 +1,17 @@
+#include "cBank.h"
+
+cBank::cBank()
+{
+}
+
+cBank::~cBank()
+{
+}
+
+void cBank::giveMoney(cPlayer toWhom, unsigned int howmuch)
+{
+}
+
+void cBank::takeMoney(cPlayer fromWhom, unsigned int howmuch)
+{
+}
--- Project3-Network/TCPServer/cBank.h
+++ Project3-Network/TCPServer/cBank.h
@@ -1,1 +1,16 @@
 #pragma once
+#include "cPlayer.h"
+
+class cBank
+{
+public:
+	cBank();
+	~cBank();
+
+	void giveMoney(cPlayer toWhom, unsigned int howmuch);
+	void takeMoney(cPlayer fromWhom, unsigned int howmuch);
+
+private:
+	unsigned int m_money;
+
+};
