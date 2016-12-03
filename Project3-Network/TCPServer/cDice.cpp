982d398b8fa9e5ae93d469ed5c76b77404ae9b5c
OmerCora <omercora@gmail.com>
2016-12-03 09:24:18 -0500

    added a doubleThrow to cDice
    for double dice rolling

--- Project3-Network/TCPServer/cDice.cpp
+++ Project3-Network/TCPServer/cDice.cpp
@@ -14,7 +14,24 @@ cDice::~cDice()
 
 int cDice::Throw()
 {
-	int diceNumber = rand() % 6;
+	int diceNumber = rand() % 6 +1;
 	std::cout << "\t cDice::Throw() diceNumber: " << diceNumber << std::endl;
 	return diceNumber;
+}
+
+bool cDice::DoubleThrow(float & diceValue)
+{
+	int diceNumber1 = rand() % 6 +1; //1..6
+	int diceNumber2 = rand() % 6 +1; //1..6
+
+	diceValue = diceNumber1 + diceNumber2;
+
+	if (diceNumber1 == diceNumber2)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
